db9405171154c16c8913f1e08b001877086e286c
SergioADJ <1461504@senati.pe>
2024-10-13 16:33:30 -0500

    FIX: agregado condicional para jquery

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -7,8 +7,16 @@ class HomeController
     public function index()
     {
         $title = "Dashboard";
-        $content = __DIR__ . '/../views/home.php'; //CONTENIDO
-        $data = "data";
-        include __DIR__ . '/../views/layouts/main.php'; //Layout principal: tiene un navbar y footer, espera un CONTENIDO
+        $content = __DIR__ . '/../views/home.php';
+
+        if ($this->isAjaxRequest()) {
+            include $content; // Solo el contenido para AJAX
+        } else {
+            include __DIR__ . '/../views/layouts/main.php'; // Layout completo para la carga inicial
+        }
+    }
+    private function isAjaxRequest()
+    {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
     }
 }

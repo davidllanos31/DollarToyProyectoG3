38bf68791643e9bb71f82b2a266904029ec22cd6
yoiber <yoiber696@gmail.com>
2024-10-18 23:24:01 -0500

    🐛 FIX: hola

--- src/views/pages/categorias/index.php
+++ src/views/pages/categorias/index.php
@@ -2,7 +2,7 @@
 $url = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $method = $_SERVER['REQUEST_METHOD'];
 
-// require_once __DIR__ . '/../../partials/header.php';
+require_once __DIR__ . '/../../partials/header.php';
 
 switch ($url[0]) {
 
@@ -15,4 +15,4 @@ switch ($url[0]) {
         break;
 }
 
-// require_once __DIR__ . '/../../partials/footer.php';
+require_once __DIR__ . '/../../partials/footer.php';

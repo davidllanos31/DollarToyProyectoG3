9a125082a6f51da39e7177dcb83bcd919dfd460d
SergioADJ <1461504@senati.pe>
2024-10-21 11:53:31 -0500

    cambios navbar

--- src/views/partials/navbar.php
+++ src/views/partials/navbar.php
@@ -1,67 +1,72 @@
-<style>    
-.sidebar {
-    background-color: #222222; 
-    flex-direction: column; 
-    padding: 20px;
-}
+<style>
+    .sidebar {
+        background-color: #222222;
+        flex-direction: column;
+        padding: 20px;
+    }
 
-.nav-link {
-    display: flex; 
-    align-items: center;
-    color: white; 
-    padding: 10px 15px; 
-    border-radius: 10px; 
-    margin-bottom: 15px;
-    transition: background-color 0.3s; 
-}
+    .nav-link {
+        display: flex;
+        align-items: center;
+        color: white;
+        padding: 10px 15px;
+        border-radius: 10px;
+        margin-bottom: 15px;
+        transition: background-color 0.3s;
+    }
 
-.nav-link:hover {
-    background-color: rgba(255, 255, 255, 0.2); 
-}
+    .nav-link:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+        color: rgb(135, 206, 250)
+    }
 
-.nav-link img {
-    margin-right: 10px;
-    width: 20px; 
-    height: auto; 
-}
-</style>  
+    .nav-link img {
+        margin-right: 10px;
+        width: 20px;
+        height: auto;
+    }
+
+    .logo {
+        margin-bottom: 24px;
+    }
+</style>
 
 <div class="sidebar">
-    <h4><img src="public/assets/images/logodollar.png" alt="Logo" class="logo"></h4>
+    <div class="logo"><img src="public/assets/images/logodollar-horizontal.png" width="150px"></div>
     <ul class="nav flex-column">
         <li class="nav-item">
             <a href="<?= BASE_URI; ?>/home" class="nav-link active">
-                <img src="<?= BASE_URI; ?>/public/assets/images/dashboard.png" alt="" style="width: 20px; height: 20px;"> DASHBOARD
+                <img src="<?= BASE_URI; ?>/public/assets/images/dashboard.png" alt="" style="width: 20px; height: 20px;"> Dashboard
             </a>
         </li>
         <li class="nav-item">
-            <a href="<?= BASE_URI; ?>/usuarios" class="nav-link" >
-            <img src="<?= BASE_URI; ?>/public/assets/images/usuarios.png" alt="" style="width: 20px; height: 20px;"> USUARIOS
+            <a href="<?= BASE_URI; ?>/usuarios" class="nav-link">
+                <img src="<?= BASE_URI; ?>/public/assets/images/usuarios.png" alt="" style="width: 20px; height: 20px;"> Usuarios
             </a>
         </li>
         <li class="nav-item">
-            <a href="<?= BASE_URI; ?>/categorias" class="nav-link" >
-            <img src="<?= BASE_URI; ?>/public/assets/images/categorias.png" alt="" style="width: 20px; height: 20px;"> CATEGORIAS
+            <a href="<?= BASE_URI; ?>/categorias" class="nav-link">
+                <img src="<?= BASE_URI; ?>/public/assets/images/categorias.png" alt="" style="width: 20px; height: 20px;"> Categorias
             </a>
         </li>
         <li class="nav-item">
-            <a href="<?= BASE_URI; ?>/ventas" class="nav-link" >
-            <img src="<?= BASE_URI; ?>/public/assets/images/ventas.png" alt="" style="width: 20px; height: 20px;"> VENTAS
+            <a href="<?= BASE_URI; ?>/ventas" class="nav-link">
+                <img src="<?= BASE_URI; ?>/public/assets/images/ventas.png" alt="" style="width: 20px; height: 20px;"> Ventas
             </a>
         </li>
         <li class="nav-item">
-            <a href="<?= BASE_URI; ?>/sedes" class="nav-link" >
-            <img src="<?= BASE_URI; ?>/public/assets/images/sedes.png" alt="" style="width: 20px; height: 20px;"> SEDES
+            <a href="<?= BASE_URI; ?>/sedes" class="nav-link">
+                <img src="<?= BASE_URI; ?>/public/assets/images/sedes.png" alt="" style="width: 20px; height: 20px;"> Sedes
             </a>
         </li>
         <li class="nav-item">
-            <a href="<?= BASE_URI; ?>/productos" class="nav-link" >
-            <img src="<?= BASE_URI; ?>/public/assets/images/productos.png" alt="" style="width: 20px; height: 20px;"> PRODUCTOS
+            <a href="<?= BASE_URI; ?>/productos" class="nav-link">
+                <img src="<?= BASE_URI; ?>/public/assets/images/productos.png" alt="" style="width: 20px; height: 20px;"> Productos
             </a>
         </li>
         <li class="nav-item">
-            <a href="<?= BASE_URI; ?>/iniciar-sesion" class="nav-link" >
-            <img src="<?= BASE_URI; ?>/public/assets/images/logout.png" alt="" style="width: 20px; height: 20px;">SALIR
+            <a href="<?= BASE_URI; ?>/iniciar-sesion" class="nav-link">
+                <img src="<?= BASE_URI; ?>/public/assets/images/logout.png" alt="" style="width: 20px; height: 20px;">SALIR
             </a>
         </li>
     </ul>

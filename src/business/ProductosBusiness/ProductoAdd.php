f205b140dbf72332ef2c3561c73e6320412f4242
yoiber <yoiber696@gmail.com>
2024-10-22 00:10:58 -0500

    ...

---
+++ src/business/ProductosBusiness/ProductoAdd.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\Business\Producto;
+
+use app\exceptions\DataException;
+use app\Interfaces\CategoriaInterface;
+use app\Interfaces\ProductoInterface;
+use app\Interfaces\SedesInterface;
+use app\Exceptions\ValidationException;
+use app\interfaces\ValidatorInterfaceActual;
+use app\Models\Producto;
+use app\Models\SedeProducto;
+
+class ProductoAdd
+{
+    public function __construct(
+        private ProductoInterface $producto,
+        private ValidatorInterfaceActual $validator,
+        private CategoriaInterface $categoria,
+        private SedesInterface $sede
+    ) {}
+
+    public function add(array $data): bool
+    {
+        if (!$this->validator->validateAdd($data)) {
+            throw new ValidationException($this->validator->getError());
+        }
+
+        if (!$this->validator->validateId($data['id_sede'])) {
+            throw new DataException('Sede no existe');
+        }
+
+        if (!$this->validator->validateId($data['id_categoria_producto'])) {
+            throw new DataException('Categoria no existe');
+        }
+
+        if (!$this->validator->validateId($data['id'])) {
+            throw new DataException('Producto no existe');
+        }
+
+        $categoria = $this->categoria->find(['id_categoria' => $data['id_categoria_producto']]);
+        $sede = $this->sede->find(['id_sede' => $data['id_sede']]);
+
+        $producto = new Producto($data['id'], $data['nombre'], $data['descripcion'], $data['precio'], $data['imagen_url'], $categoria[0]);
+
+        $sedeProducto = new SedeProducto($data['id_sede'], 0, $data['stock_disponible']);
+
+        return $this->producto->save($producto, $sedeProducto);
+    }
+}
--- src/views/pages/productos/edit.php
+++ src/views/pages/productos/edit.php
@@ -1,18 +1,15 @@
-<form id="productoForm" style="display: flex; flex-direction: column;">
+ <form action="/productos/update/<?php echo $producto->getId(); ?>" method="POST">
     <label for="nombre">Nombre</label>
-    <input type="text" name="nombre" id="nombre" required>
+    <input type="text" name="nombre" id="nombre" value="<?php echo $producto->getNombre(); ?>" required>
 
     <label for="descripcion">Descripción</label>
-    <input type="text" name="descripcion" id="descripcion" required>
+    <input type="text" name="descripcion" id="descripcion" value="<?php echo $producto->getDescripcion(); ?>" required>
 
     <label for="precio">Precio</label>
-    <input type="number" step="0.01" name="precio" id="precio" required>
+    <input type="number" name="precio" id="precio" value="<?php echo $producto->getPrecio(); ?>" required>
 
-    <label for="img">Imagen</label>
-    <input type="file" name="img" id="img" required>
-
-    <label for="categoria">Categoría</label>
-    <select name="id_categoria_producto" id="categoria" required>
+    <label for="id_categoria_producto">Categoría</label>
+    <select name="id_categoria_producto" id="id_categoria_producto" required>
         <?php foreach ($categorias as $categoria): ?>
             <option value="<?= $categoria['id']; ?>" >
                 <?= $categoria['nombre']; ?>
@@ -23,24 +20,24 @@
         <?php endforeach; ?>
     </select>
 
-    <label for="sede">Sede</label>
-    <select name="id_sede" id="sede" required>
+    <label for="id_sede">Sede</label>
+    <select name="id_sede" id="id_sede" required>
         <?php foreach ($sedes as $sede): ?>
             <option value="<?= $sede['id']; ?>"><?= $sede['nombre']; ?></option>
         <?php endforeach; ?>
     </select>
 
     <label for="stock">Stock Disponible</label>
-    <input type="number" name="stock_disponible" id="stock" required>
+    <input type="number" name="stock_disponible" id="stock" value="<?php echo $producto->getStockDisponible(); ?>" required>
 
-    <button type="submit">Guardar</button>
+    <button type="submit">Actualizar</button>
 </form>
 <script>
-    document.getElementById('categoria').addEventListener('change', function() {
+    document.getElementById('id_categoria_producto').addEventListener('change', function() {
         const formData = new FormData(this.form);
         console.log(formData);
         $.ajax({
-            url: '/DollarToyProyectoG3/productos/crear',
+            url: '/DollarToyProyectoG3/productos/update',
             method: 'POST',
             data: formData,
             success: function (response) {
@@ -49,7 +46,7 @@
                 window.location.href = '/DollarToyProyectoG3/productos';
             },
             error: function () {
-                alert("Error al guardar el producto.");
+                alert("Error al actualizar el producto.");
             }
         });
     });
